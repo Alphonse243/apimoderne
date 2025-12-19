1baa3e105cf5cc4e71a00213ae90167174f8976b
Alphonse243 <alphonsekatumbescience@gmail.com>
2025-12-19 12:17:15 +0200

    config pour le shop change le nom de la base de donnee

--- database/seeders/ProduitSeeder.php
+++ database/seeders/ProduitSeeder.php
@@ -15,7 +15,7 @@ $capsule = new Capsule;
 $capsule->addConnection([
     'driver' => 'mysql',
     'host' => $_ENV['DB_HOST'] ?? 'localhost',
-    'database' => $_ENV['DB_DATABASE'] ?? 'blog-php-moderne',
+    'database' => $_ENV['DB_DATABASE'] ?? 'shop_vanilla_php',
     'username' => $_ENV['DB_USERNAME'] ?? 'root',
     'password' => $_ENV['DB_PASSWORD'] ?? '',
     'charset' => 'utf8mb4',
